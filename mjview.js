46a510887ff2b1cc56ec6f98f8a099fb39db2829
Ryusei Yamaguchi <mandel59@gmail.com>
2011-11-20 20:42:36 +0900

    progress

--- mjview.js
+++ mjview.js
@@ -1,3 +1,14 @@
+function charWidthRegulation(str){
+  var len = str.length;
+  for(var i = 0; i < len; i++){
+    c = str.charCodeAt(i);
+    if(0xFF00 < c && c < 0xFF5F) {
+      str = str.substring(0, i) + String.fromCharCode(c - 0xFF00 + 0x20)
+          + str.substring(i+1);
+    }
+  }
+  return str;
+}
 function zeronum(n, digits){
   return (new Array(digits).join("0") + n.toString()).slice(-digits);
 }
@@ -27,7 +38,7 @@ function IsIVSSupported() {
       return true;
     if($.browser.opera && navigator.userAgent.indexOf("Win") != -1)
       return true;
-    if($.browser.msie && parseInt($.browser.version.match(/\d+/)) > 9 )
+    if($.browser.msie && parseInt($.browser.version.match(/\d+/),10) > 9 )
       return true;
   }
   return false;
@@ -67,6 +78,7 @@ function ExGlyph(url, exglyph){
 }
 
 function ParseQuery(val){
+  val = charWidthRegulation(val);
   var qs = val.replace("\u3000", " ").split(" ");
   var query = [];
   for(var i = 0; i < qs.length; i++){
@@ -77,10 +89,31 @@ function ParseQuery(val){
       continue;
     }
     if(q.substring(0,2) == "MJ") {
-      query.push(["MJ", "MJ" + zeronum(parseInt(q.substring(2), 10), 6)]);
+      q.match(/^MJ(\d+)(-(\d+))?$/);
+      if(RegExp.$1) {
+        if(RegExp.$3) {
+          var mj1 = zeronum(parseInt(RegExp.$1, 10), 6);
+          var l = 6-(RegExp.$3).length;
+          var mj2 = parseInt(mj1.substring(0,l) + RegExp.$3, 10);
+          query.push(["MJ", parseInt(RegExp.$1, 10), mj2]);
+        } else {
+          query.push(["MJ", "MJ" + zeronum(parseInt(RegExp.$1, 10), 6)]);
+        }
+      }
       continue;
     }
     var q0 = q.charAt(0);
+    if('\u2F00' <= q0 && q0 < '\u2FD6') {
+      q.match(/^.(\[(\d+)\])?(-?(\d+)?-?(\d+)?)$/);
+      var radical = q0.charCodeAt(0) - 0x2F00 + 1;
+      var radical2 = parseInt(RegExp.$2);
+      var others = RegExp.$3 ? RegExp.$3 : "";
+      if(radical2 && (radical !== radical2)) {
+        continue;
+      }
+      q = "[" + radical + "]" + others;
+      q0 = '[';
+    }
     switch(q0) {
       case '[':
         q.match(/^\[(\d+)\]-?(\d+)?-?(\d+)?$/);
@@ -97,14 +130,27 @@ function ParseQuery(val){
         }
         continue;
       case '<': case '=': case '>':
-        var v = q.substring(1).split("-");
-        if(v.length == 2) {
-          query.push(["Strk", parseInt(v[0]), parseInt(v[1])]);
-        } else query.push(["Strk", q]);
+        q.match(/^((=|<|>|<=|>=)(\d+)|=(\d+)-(\d+))$/);
+        if(RegExp.$2) {
+          query.push(["Strk", RegExp.$2 + RegExp.$3]);
+        } else if(RegExp.$4) {
+          query.push(["Strk", parseInt(RegExp.$4, 10), parseInt(RegExp.$5, 10)]);
+        }
+        continue;
+      case '#':
+        qs[i].match(/^#(\w+)$/);
+        if(RegExp.$1) {
+          query.push(["HS", RegExp.$1]);
+        }
         continue;
       case 'U':
-        q.match(/^U\+?([0-9A-F]{4,5})$/);
-        if(RegExp.$1) {
+        q.match(/^U\+?([0-9A-F]{4,5})(-([0-9A-F]+))?$/);
+        if (RegExp.$2) {
+          var ucs = parseInt(RegExp.$1, 16);
+          var l = (RegExp.$3).length * 4;
+          var ucs2 = ((ucs >> l) << l) + parseInt(RegExp.$3, 16);
+          query.push(["UCS", ucs, ucs2]);
+        }else if(RegExp.$1) {
           var ucs = parseInt(RegExp.$1, 16);
           query.push(["UCS", ucs]);
         }
@@ -122,7 +168,12 @@ function XPathQuery(arr, nodes){
   for(var i = 0; i < arr.length; i++){
     switch(arr[i][0]) {
       case "MJ":
-        str += "[ci:MJGlyphName/text()=\"" + arr[i][1] + "\"]";
+        if(arr[i][2]) {
+          str += "[number(substring(ci:MJGlyphName/text(),3))>=" + arr[i][1]
+               + " and number(substring(ci:MJGlyphName/text(),3))<=" + arr[i][2] + "]";
+        } else {
+          str += "[ci:MJGlyphName/text()=\"" + arr[i][1] + "\"]";
+        }
         break;
       case "Strk":
         if(arr[i][2]) {
@@ -147,7 +198,12 @@ function XPathQuery(arr, nodes){
         str += "[ci:Readings/ci:Reading/text()=\"" + arr[i][1] + "\"]";
         break;
       case "UCS":
-        str += "[ci:UCS/@ci:ucs=" + arr[i][1] + "]";
+        if(arr[i][2]) str += "[ci:UCS/@ci:ucs>=" + arr[i][1]
+                           + " and ci:UCS/@ci:ucs<=" + arr[i][2] + "]";
+        else str += "[ci:UCS/@ci:ucs=" + arr[i][1] + "]";
+        break;
+      case "HS":
+        str += "[contains(ci:HeiseiMincho/text(), \"" + arr[i][1] + "\")]";
         break;
       case "Str":
         str += "[contains(ci:UCS/text(), \"" + arr[i][1]
@@ -195,13 +251,16 @@ function showMJInfo(xml, glyph, output) {
   var xpath = XPathQuery([["MJ", mj]]);
   var chr = xml.evaluate(xpath, xml, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
   $("<div/>").append(glyph.clone()).attr("id", "detail_glyph").appendTo(output);
-  var infolist = $("<dl/>");
+  var urllist = $("<ul class='links'/>");
+  $("<li/>").append($("<a target='_blank'>PNG</a>").attr("href", MJImageURI(mj))).appendTo(urllist);
+  $("<li/>").append($("<a target='_blank'>SVG</a>").attr("href", MJSVGURI(mj))).appendTo(urllist);
+  urllist.appendTo(output);
   var info = $(chr).clone();
   var indices = xml.evaluate(xpath+"//ci:Index", xml, nsResolver, XPathResult.ANY_TYPE, null);
   var index;
   while(index = indices.iterateNext()) {
-    var radical = parseInt(index.getAttributeNS(CharInfoNS, "radical"));
-    var strokes = parseInt(index.getAttributeNS(CharInfoNS, "strokes"));
+    var radical = parseInt(index.getAttributeNS(CharInfoNS, "radical"),10);
+    var strokes = parseInt(index.getAttributeNS(CharInfoNS, "strokes"),10);
     var txt = KangXiRadical(radical) + "[" + radical + "]-" + strokes;
     $("<div/>").addClass("kangxi_index").text(txt).appendTo(output);
   }
@@ -209,7 +268,7 @@ function showMJInfo(xml, glyph, output) {
   return output;
 }
 
-function showGlyphs(result, output, bind) {
+function showGlyphs(result, output, bind, stop) {
   if(!output) output = $("<div/>");
   var MAX = 50;
   var itr;
@@ -224,24 +283,45 @@ function showGlyphs(result, output, bind) {
     output.showGlyphsTimerID = setTimeout(function(){showGlyphs(result, output, bind);}, 10);
   } else {
     output.showGlyphsTimerID = null;
+    if(typeof(stop) === 'function') stop();
   }
   return output;
 }
 
 $(document).ready(function(){
+  var _ = function(locale){
+    var __ = {
+      'ja': {
+        "Search": "検索",
+        "Stop": "停止",
+        "<p>now loading...</p>": "<p>読み込み中...</p>",
+      },
+    }
+    return function(s) {
+      var t = __[locale][s];
+      return t ? t : s;
+    };
+  }($('html').attr('lang'));
+
   var input = $("#input");
   var output = $("#output");
   var detail = $("#detail");
+  input.append(_("<p>now loading...</p>"));
   var xhr = $.ajax("data/mjcharinfo.xml").done(function(xml){
+    if( !$.fontAvailable("MJMincho") ) {
+      $("#message").append(_("<p><a href='http://ossipedia.ipa.go.jp/ipamjfont/download.html'>IPAmj明朝</a>のインストールを推奨します。</p>"));
+    }
     var inputbox = $("<input type='text'/>");
-    var button = $("<input type='submit' value='Search'/>");
+    var button = $("<input type='submit'/>").attr("value", _("Search"));
     var form = $("<form/>").append(inputbox).append(button).submit(function(){return false;});
     form.submit(function(){
       if(output.showGlyphsTimerID) {
         clearTimeout(output.showGlyphsTimerID);
         output.showGlyphsTimerID = null;
+        button.attr("value", _("Search"));
         return;
       }
+      button.attr("value", _("Stop"));
       var val = inputbox.val();
       var query = ParseQuery(val);
       if(query.length > 0) {
@@ -257,7 +337,7 @@ $(document).ready(function(){
             "click": function(){
               detail.empty().append(showMJInfo(xml, $(this)));
             },
-          });
+          }, function() {button.attr("value", _("Search"));});
         }
       }
     });
